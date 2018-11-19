cdb1025fe1e489eb6adb6cf4e67bc38b5010af9f
fish ugly <jiaxiaole_2009@163.com>
2018-11-19 23:31:53 +0800

    成功的返回了数据，用微信的api发了get请求

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,16 @@
 Page({
   onLoad(){
-    console.log("hello world")
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', //仅为示例，并非真实的接口地址
+      data: {
+        city: '上海市',
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log(res.data)
+      }
+    })
   }
 })
